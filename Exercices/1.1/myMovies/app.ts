e8212a074f1bff54437e5a371374d989103a1055
Azrid2004 <louis.levander@gmail.com>
2026-08-28 14:37:13 +0200

    new : ex1.1

--- Exercices/1.1/myMovies/app.ts
+++ Exercices/1.1/myMovies/app.ts
@@ -1,6 +1,5 @@
 import express from "express";
 
-import usersRouter from "./routes/users";
 import filmsRouter from "./routes/films";
 
 const app = express();
@@ -8,7 +7,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/users", usersRouter);
 app.use("/films", filmsRouter);
 
 export default app;
